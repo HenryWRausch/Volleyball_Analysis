97aedddf7c29a3830ce37c8c47f2738bb8c03ffc
Henry <henrywrausch@gmail,com>
2024-09-05 16:10:00 -0400

    Added the ability to create team table entries, and run generic select queries to test write functionality
    Amended to fix error where connection to database
    was closed prematurely

--- databaseUtilities.py
+++ databaseUtilities.py
@@ -25,6 +25,58 @@ def create_table(conn: sqlite3.Connection, sql_command: str):
     with conn:
         conn.execute(sql_command)
 
+def generic_select_query(conn: sqlite3.Connection, query: str) -> list[tuple[any]]:
+    """
+    Execute a SELECT query on the SQLite database and return the results.
+
+    Parameters:
+    db_file (str): The path to the SQLite database file.
+    query (str): The SQL SELECT query to execute.
+    params (tuple): Parameters to be used in the SQL query (default is an empty tuple).
+
+    Returns:
+    List[Tuple[Any]]: Results of the query as a list of tuples.
+    """
+    results = []
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+
+    
+    except sqlite3.Error as e:
+        print(f"SQLite error: {e}")
+    
+    return results
+
+def add_team(conn: sqlite3.Connection, team_name: str):
+    """
+    Add a new team to the team table in the SQLite database.
+
+    Parameters:
+    conn (sqlite3.Connection): Connection object to the SQLite database.
+    team_name (str): The name of the team to be inserted.
+
+    Raises:
+    sqlite3.Error: If an SQLite error occurs during the operation.
+    """
+    try:
+        cursor = conn.cursor()
+        
+        command = f'''
+        INSERT INTO team (team_name)
+        VALUES ('{team_name}');
+        '''
+        cursor.execute(command)
+
+        conn.commit()
+        
+    except sqlite3.Error as e:
+        print(f"SQLite error: {e}")
+
+
 team_table_string = '''
  CREATE TABLE IF NOT EXISTS team (
         team_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -74,6 +126,9 @@ tables = [team_table_string,
           attack_table_string,
           defense_table_string]
 
+
+
+#DEBUG
 def main():
     database = "testDatabase.db"  # SQLite database file
 
@@ -82,6 +137,8 @@ def main():
     for table in tables:
         create_table(conn, table)
 
+    add_team(conn, 'Test_Team')
+    print(generic_select_query(conn, 'SELECT * FROM team;'))
 
 
     conn.close()
